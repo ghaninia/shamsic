d2da1afc5e78afa2d3e9a3a94a62bca8ece5f05f
ghaninia <mohamadaminghaninia@gmail.com>
2022-07-10 17:43:34 +0430

    feature : append test for single validations

---
+++ src/Validations/Single/StepValue.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GhaniniaIR\SolarCron\Validations\Single;
+
+use GhaniniaIR\SolarCron\Validations\Single\Interfaces\ValidationContract;
+
+class StepValue implements ValidationContract
+{
+
+    public function passes($value): bool
+    {
+        $values = explode("/", $value);
+
+        if (count($values) !== 2) {
+            return false;
+        }
+
+        $firstValue = $values[0];
+        $secondValue = (int) $values[1];
+
+        if ($secondValue <= 0) {
+            return false;
+        }
+
+        if ($firstValue != "*") {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function message(): string
+    {
+        return "The value must be between {$this->min} and {$this->max}";
+    }
+}
---
+++ tests/units/Validations/Single/StepValueTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use GhaniniaIR\SolarCron\Validations\Single\StepValue;
+
+class StepValueTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function stepValue()
+    {
+        $result = (new StepValue())->passes("*/2");
+        $this->assertTrue($result);
+
+        $result = (new StepValue())->passes("*/2/3");
+        $this->assertFalse($result);
+
+        $result = (new StepValue())->passes("*/0");
+        $this->assertFalse($result);
+
+        $result = (new StepValue())->passes("0/*");
+        $this->assertFalse($result);
+    }
+}
